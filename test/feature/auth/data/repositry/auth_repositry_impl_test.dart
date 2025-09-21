42e806c2dfa01a8278f7c0f512d81d7a343ac120
mariam2MOHMED <mariammohmed.25720@gmail.com>
2025-09-21 19:42:25 +0300

    auth test2

--- test/feature/auth/data/repositry/auth_repositry_impl_test.dart
+++ test/feature/auth/data/repositry/auth_repositry_impl_test.dart
@@ -1,17 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:tracking_app/feature/auth/data/data_source/remote/auth_remote_data_source.dart';
-import 'package:tracking_app/feature/auth/data/repositry/auth_repositry_impl.dart';
-
-import 'auth_repositry_impl_test.mocks.dart';
+// import 'package:tracking_app/feature/auth/data/repositry/auth_repositry_impl.dart';
+//
+// import 'auth_repositry_impl_test.mocks.dart';
 @GenerateMocks([AuthRemoteDataSource])
 void main() {
- late MockAuthRemoteDataSource mockAuthRemoteDataSource;
- late AuthRepositryImpl authRepositoryImp;
- setUp((){
-  mockAuthRemoteDataSource=MockAuthRemoteDataSource();
-  authRepositoryImp=AuthRepositryImpl();
- });
+ // late MockAuthRemoteDataSource mockAuthRemoteDataSource;
+ // late AuthRepositryImpl authRepositoryImp;
+ // setUp((){
+ //  mockAuthRemoteDataSource=MockAuthRemoteDataSource();
+ //  authRepositoryImp=AuthRepositryImpl();
+ // });
 
  group("Auth Repositry",(){});
 }
