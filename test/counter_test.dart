54f99055bbd6ff2327c878601a94bd4f325ff1f6
Aya Mohamed <aya44saber@gmail.com>
2025-09-21 22:43:33 +0300

    add some tests

---
+++ test/counter_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tracking_app/counter.dart';
+
+
+void main() {
+  group('Counter', () {
+    test('initial value should be 0', () {
+      final counter = Counter();
+      expect(counter.value, 0);
+    });
+
+    test('increment increases value by 1', () {
+      final counter = Counter();
+      counter.increment();
+      expect(counter.value, 1);
+    });
+
+    test('decrement decreases value by 1', () {
+      final counter = Counter();
+      counter.decrement();
+      expect(counter.value, -1);
+    });
+  });
+}
