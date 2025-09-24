2b019ad6ebeb2ff8985c844286d60eb6036a517b
Aya921 <aya44saber@gmail.com>
2025-09-24 13:06:23 +0300

    fix conflicts

--- lib/core/responsive/device_utils.dart
+++ lib/core/responsive/device_utils.dart
@@ -14,7 +14,7 @@ class DeviceUtils {
     return context.scaleWidth >= 1200;
   }
 
-  static T  valueDecider<T>(
+  static T valueDecider<T>(
     BuildContext context, {
     required T onMobile,
     T? onTablet,
