48395dd98d4d76e4859586460b33bc481010ab5e
Rana_Gebril <ranagebril5@gmail.com>
2025-09-27 17:35:34 +0300

    add profil feature base

---
+++ lib/feature/profile/api/client/profile_api_services.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:tracking_app/core/constants/end_points_constants.dart';
+
+part 'profile_api_services.g.dart';
+
+@RestApi(baseUrl: EndPointsConstants.baseUrl)
+@injectable
+abstract class ProfileApiServices {
+  @factoryMethod
+  factory ProfileApiServices(Dio dio) = _ProfileApiServices;
+
+}
