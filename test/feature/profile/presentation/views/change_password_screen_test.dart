1ae468d8fccfe7e4276f3971f718ed5bc0005303
MahmoodEbrahim <m7kmod7k2003@gmail.com>
2025-09-28 00:01:07 +0300

    add widget and unit test

--- test/feature/profile/presentation/views/change_password_screen_test.dart
+++ test/feature/profile/presentation/views/change_password_screen_test.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:tracking_app/core/api_result/result.dart';
+import 'package:tracking_app/feature/profile/api/models/change_password_response.dart';
 import 'package:tracking_app/feature/profile/domain/use_case/use_case.dart';
 import 'package:tracking_app/feature/profile/presentation/views/screens/change_password.dart';
 
@@ -12,22 +14,27 @@ void main() {
   late MockChangePasswordUseCase mockUseCase;
 
   setUp(() {
-    sl.reset();
     mockUseCase = MockChangePasswordUseCase();
+
+    sl.reset();
+
     sl.registerLazySingleton<ChangePasswordUseCase>(() => mockUseCase);
   });
 
-  testWidgets("renders ChangePasswordScreen with fields and button",
-          (tester) async {
-        await tester.pumpWidget(
-          const MaterialApp(
-            home: ChangePasswordScreen(),
-          ),
-        );
+  testWidgets('renders ChangePasswordScreen with fields and button', (tester) async {
+    when(() => mockUseCase.call(any())).thenAnswer(
+          (_) async => SucessResult(ChangePasswordResponse(message: "Password changed")),
+    );
 
-        expect(find.text("Current password"), findsOneWidget);
-        expect(find.text("New password"), findsOneWidget);
-        expect(find.text("Confirm new password"), findsOneWidget);
-        expect(find.text("Submit"), findsOneWidget);
-      });
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: ChangePasswordScreen(),
+      ),
+    );
+
+    expect(find.text("Current password"), findsOneWidget);
+    expect(find.text("New password"), findsOneWidget);
+    expect(find.text("Confirm new password"), findsOneWidget);
+    expect(find.text("Change Password"), findsOneWidget);
+  });
 }
