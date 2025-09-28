6112e884564c967548628a8b82c4b34a74a5acc4
Aya921 <aya44saber@gmail.com>
2025-09-28 08:30:10 +0300

    add token interceptor

--- lib/config/di/models/dio_model.dart
+++ lib/config/di/models/dio_model.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:tracking_app/config/di/di.dart';
+import 'package:tracking_app/config/network/token_interceptor.dart';
 import 'package:tracking_app/core/constants/constants.dart';
 import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
 
@@ -14,6 +15,7 @@ abstract class DioModule {
     dio.interceptors.add(getIt.get<PrettyDioLogger>());
     dio.options.headers = {Constants.contentType: Constants.appJson};
     dio.interceptors.add(DioCacheInterceptor(options: cacheOptions));
+     dio.interceptors.add(TokenInterceptor());
 
     return dio;
   }
