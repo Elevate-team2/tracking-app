15a51361978a7f80c4ab2ca0a7ca90c68ecdb9aa
mariam2MOHMED <mariammohmed.25720@gmail.com>
2025-09-28 13:16:02 +0300

    safe call

--- lib/core/safe_api_call/safe_api_call.dart
+++ lib/core/safe_api_call/safe_api_call.dart
@@ -1,13 +1,22 @@
 import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:tracking_app/core/api_error/api_error.dart';
 import 'package:tracking_app/core/api_result/result.dart';
 
-Future<Result<T>> safeApiCall<T>(Future<T> Function() call) async {
+import '../firebase_error/firebase_error.dart';
+
+Future<Result<T>> safeCall<T>(Future<T> Function() call) async {
   try {
     final response = await call();
     return SucessResult(response);
-  } on DioException catch (dioError) {
-    return FailedResult(ServerFailure.fromDioError(dioError).errorMessage);
+  } on Exception catch (error) {
+    if (error is DioException) {
+      return FailedResult(ServerFailure.fromDioError(error).errorMessage);
+    } else if (error is FirebaseException) {
+      return FailedResult(FirebaseErrorHandler.handle(error).errorMessage);
+    } else {
+      return FailedResult(error.toString());
+    }
   } catch (error) {
     return FailedResult(error.toString());
   }
