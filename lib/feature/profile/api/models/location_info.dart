c5f6ccada34d061e1df1b1cae5c0960dd3759a9d
shahd523 <shahdehab798@gmail.com>
2025-09-29 19:40:27 +0300

    made model files for profile

---
+++ lib/feature/profile/api/models/location_info.dart
@@ -0,0 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'location_info.g.dart';
+
+@JsonSerializable()
+class LocationInfo {
+  @JsonKey(name: "country")
+  final String? country;
+  @JsonKey(name: "createdAt")
+  final String? createdAt;
+
+  LocationInfo ({
+    this.country,
+    this.createdAt,
+  });
+
+  factory LocationInfo.fromJson(Map<String, dynamic> json) {
+    return _$LocationInfoFromJson(json);
+  }
+
+  Map<String, dynamic> toJson() {
+    return _$LocationInfoToJson(this);
+  }
+}
+
